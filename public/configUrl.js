2a7a44b7c0beccde8f04b087a408f479a467f8fd
zhangrui <zhangrui_web@163.com>
2019-12-18 10:43:55 +0800

    新增初始化主题配置

--- public/configUrl.js
+++ public/configUrl.js
@@ -48,6 +48,7 @@ const configItem = {
   is_zsj: '0', //首页综合统计显示本级还是下级'1'本级，'0'下级
   is_area: '5', // 刑事案件案件类别根据不同地区调用不同的接口；1表示牡丹江,2表示平乐,0表示主线，5表示达拉特旗
   is_ssds: 'hb', // 接处警报警类别的所属地市dltq
+  startTheme:true,//初始化主题深色版本true,浅色版本false,
 };
 // 开发模式地址
 const developConfigUrl = {
