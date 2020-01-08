0bea3d8a5081bddc1915c5a4a03d73c2ddd8b7e3
姜翰墨 <1559481194@qq.com>
2020-01-08 14:55:14 +0800

    配置文件地区参数修改

--- public/configUrl.js
+++ public/configUrl.js
@@ -49,7 +49,7 @@ const configItem = {
   clearHome: false, //是否显示三清调度首页（鹤壁定制）
   is_zsj: '0', //首页综合统计显示本级还是下级'1'本级，'0'下级
   is_area: '2', // 刑事案件案件类别根据不同地区调用不同的接口；1表示牡丹江,2表示平乐,0表示主线，5表示达拉特旗
-  is_ssds: 'dltq', // 接处警报警类别的所属地市dltq
+  is_ssds: 'pl', // 接处警报警类别的所属地市dltq
   startTheme:true,//初始化主题深色版本true,浅色版本false,
   is_sawpbb:'1', // 涉案物品后台数据版本;1为1.0版本,2为2.0版本;
 };
